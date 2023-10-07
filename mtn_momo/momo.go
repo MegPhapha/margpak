5c60d49bdb38390b5fe474da100481190cd78ad2
Margaret Amanfu <margaretfafa@gmail.com>
2023-10-06 21:42:26 -0700

    mtn momo practice

--- mtn_momo/momo.go
+++ mtn_momo/momo.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"margpak/mathformula"
+	"math/rand"
 )
 
 func main() {
@@ -12,6 +13,9 @@ func main() {
 	var confirmed string
 	var payment float64
 	var reference string
+	var availableBalance float64
+	//var names string
+	//var transctionId rand.Intn
 	fmt.Println("Input short code (*170#)")
 	fmt.Println("1) Transfer Money")
 	fmt.Println("2) MoMoPay & Pay Bill")
@@ -46,30 +50,24 @@ func main() {
 		// transfering money to momo user
 		if options == 1 {
 
-			
 			fmt.Print("Enter mobile number(10 digits): ")
 			_, err := fmt.Scan(&number)
 			if err != nil || len(number) != 10 {
 				fmt.Println("Invalid mobile number.")
 				return
-				
-			
 			}
-				fmt.Print("Confirm number: ")
-				_, err = fmt.Scan(&confirmed)
-				if err != nil {
-					fmt.Println(err)
-				}
-			
+			fmt.Print("Confirm number: ")
+			_, err = fmt.Scan(&confirmed)
+			if err != nil {
+				fmt.Println(err)
+			}
+			// Mobile number must be same as confirmed number
+			if number != confirmed {
+				fmt.Println("Number mismatched, try again.")
+				return
+			}
 
-
-				// Mobile number must be same as confirmed number
-				if number != confirmed {
-					fmt.Println("Number mismatched")
-
-				}
-
-			
+			//names = Kofi, Ama, 
 
 			fmt.Println("Enter amount: ")
 			fmt.Scan(&payment)
@@ -77,37 +75,35 @@ func main() {
 			//Formula for current balance in the math formula folder/file
 			// func CurrentBalance(availablebal float64, payment float64) (float64, error) {
 			// 	if payment > availablebal {
-			// 		return 0, fmt.Errorf("insufficient balance")
-
+			// 		return 0, fmt.Errorf("insufficient balance"
 			// 	}
-
 			// 	current_bal := availablebal - payment
-
 			// 	return current_bal, nil
 			currentBalance, err := mathformula.CurrentBalance(3000.10, payment)
 			if err != nil || payment <= 0 {
 				fmt.Println("Invalid amount.")
 
-				if payment > currentBalance {
-					fmt.Println("Insufficient balance.")
+				if payment > availableBalance {
+					fmt.Println("insufficient balance")
+					return
+
 				}
 			}
-
 			fmt.Println("Enter reference: ")
 			fmt.Scan(&reference)
 			//initializing transfer
 			//currentBalance -= payment
 			fmt.Println("Your current balance is:", currentBalance)
 
-			fmt.Println("Payment for GHS", payment, "to", number, "Current Balance:", currentBalance, "Reference:", reference)
-			return
+			fmt.Println("Payment for GHS", payment, "to", number, "with Transaction ID:", rand.Intn(1000000000055555), "Current Balance:", currentBalance, "Reference:", reference)
 
 			//} else if options == 2 {
 
 		}
+		return
 
 	case 2:
-		//	MoMoPay & Pay Bill option
+		//	MoMoPay & Pay Bill options
 		fmt.Println("MoMoPay & Pay Bill")
 		fmt.Println("1) MoMoPay")
 		fmt.Println("2) Pay Bill")
@@ -115,6 +111,7 @@ func main() {
 		fmt.Println("0) Back")
 
 	case 3:
+		// Airtime and bundles options
 		fmt.Println("Airtime & Bundles")
 		fmt.Println("1) Airtime")
 		fmt.Println("2) Internet Bundles")
@@ -124,12 +121,14 @@ func main() {
 		fmt.Println("0) Back")
 
 	case 4:
+		// Allow Cash Out options
 		fmt.Println("Allow Cash Out")
 		fmt.Println("1) Yes")
 		fmt.Println("2) No")
 		fmt.Println("0) Back")
 
 	case 5:
+		// Financial Services options
 		fmt.Println("Finanacial Services")
 		fmt.Println("1) Bank Services")
 		fmt.Println("2) Savings")
@@ -140,6 +139,7 @@ func main() {
 		fmt.Println("0) Back")
 
 	case 6:
+		//My Wallet options
 		fmt.Println("My Wallet")
 		fmt.Println("1) MoMo User")
 		fmt.Println("2) Non MoMo User")
